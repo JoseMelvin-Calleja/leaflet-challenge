60d39ea6607c9ae821475da10e572341720c63fe
JoseMelvin-Calleja <jcalleja@bates.edu>
2023-05-30 14:24:17 -0400

    added code

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,2 +1,51 @@
 const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
+d3.json(queryUrl).then(function (data) {
+    console.log(data);
+    features(data.features);
+  });
+  
+function markerSize(magnitude) {
+    return magnitude * 100;
+};
+
+function chooseColor(depth){
+    if (depth < 10) return "#00FF00";
+    else if (depth < 30) return "greenyellow";
+    else if (depth < 50) return "yellow";
+    else if (depth < 70) return "orange";
+    else if (depth < 90) return "orangered";
+    else return "#FF0000";
+};
+  
+function features(data) {
+  
+    function onEachFeature(feature, layer) {
+      layer.bindPopup(`<h3>Location: ${feature.properties.place}</h3><hr><p>Date: ${new Date(feature.properties.time)}</p><p>Magnitude: ${feature.properties.mag}</p><p>Depth: ${feature.geometry.coordinates[2]}</p>`);
+    };
+  
+    var earthquakes = L.geoJSON(data, {
+      onEachFeature: onEachFeature,
+  
+      // Point to layer used to alter markers
+      pointToLayer: function(feature, latlng) {
+  
+        // Determine the style of markers based on properties
+        var markers = {
+          radius: markerSize(feature.properties.mag),
+          fillColor: chooseColor(feature.geometry.coordinates[2]),
+          fillOpacity: 0.7,
+          color: "black",
+          stroke: true,
+          weight: 0.5
+        }
+        return L.circle(latlng,markers);
+      }
+    });
+  
+    // Send our earthquakes layer to the createMap function/
+    createMap(earthquakes);
+};
+  
+
+  
