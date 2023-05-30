0ec5d74199a01cf8b5557776be7f124cbcb792b6
JoseMelvin-Calleja <jcalleja@bates.edu>
2023-05-30 15:21:58 -0400

    added code and README

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,15 +1,15 @@
 const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-d3.json(queryUrl).then(function (data) {
+d3.json(url).then(function (data) {
     console.log(data);
     features(data.features);
   });
   
-function markerSize(magnitude) {
-    return magnitude * 100;
+function marker(magnitude) {
+    return magnitude * 10000;
 };
-
-function chooseColor(depth){
+         
+function color(depth){
     if (depth < 10) return "#00FF00";
     else if (depth < 30) return "greenyellow";
     else if (depth < 50) return "yellow";
@@ -27,17 +27,15 @@ function features(data) {
     var earthquakes = L.geoJSON(data, {
       onEachFeature: onEachFeature,
   
-      // Point to layer used to alter markers
       pointToLayer: function(feature, latlng) {
   
-        // Determine the style of markers based on properties
         var markers = {
-          radius: markerSize(feature.properties.mag),
-          fillColor: chooseColor(feature.geometry.coordinates[2]),
-          fillOpacity: 0.7,
+          radius: marker(feature.properties.mag),
+          fillColor: color(feature.geometry.coordinates[2]),
+          fillOpacity: 0.75,
           color: "black",
           stroke: true,
-          weight: 0.5
+          weight: 1
         }
         return L.circle(latlng,markers);
       }
@@ -46,6 +44,36 @@ function features(data) {
     // Send our earthquakes layer to the createMap function/
     createMap(earthquakes);
 };
+
+function createMap(earthquakes) {
+
+    var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    })
+  
+    var myMap = L.map("map", {
+      center: [
+        37.09, -95.71
+      ],
+      zoom: 5,
+      layers: [street, earthquakes]
+    });
+  
+    var legend = L.control({position: "bottomright"});
+    legend.onAdd = function() {
+      var div = L.DomUtil.create("div", "info legend"),
+      depth = [-10, 10, 30, 50, 70, 90];
+  
+      div.innerHTML += "<h3 style='text-align: center'>Depth</h3>"
+  
+      for (var i = 0; i < depth.length; i++) {
+        div.innerHTML +=
+        '<i style="background:' + chooseColor(depth[i] + 1) + '"></i> ' + depth[i] + (depth[i + 1] ? '&ndash;' + depth[i + 1] + '<br>' : '+');
+      }
+      return div;
+    };
+    legend.addTo(myMap)
+  };
   
 
   
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # leaflet-challenge
+
+In this challenge, we use LeafletJS to create a bubble map of earthquakes using data from the USGS GeonJSON. Here, we use the color to show the depth of the earthqakes, and then, we used the sixe of the circles to show the magnitude.
